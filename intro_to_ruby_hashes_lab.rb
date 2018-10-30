95b22174d4055211bd268ba3103ac0de78f851cb
John Martinez <jmartinez729@gmail.com>
2018-10-30 21:02:41 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ end
 def monopoly_with_third_tier
 	monopoly = {
 	  :railroads => {
-	    :pieces => "4", 
+	    :pieces => 4, 
 	    :names => {
 	      :reading_railroad => {}, 
 	      :pennsylvania_railroad => {}, 
@@ -38,13 +38,13 @@ def monopoly_with_third_tier
 	      :shortline => {}
 	    }, 
 	   :rent_in_dollars => {
-	     :one_piece_owned => "25", 
-	     :two_pieces_owned => "50", 
-	     :three_pieces_owned => "100", 
-	     :four_pieces_owned => "200"
+	     :one_piece_owned => 25, 
+	     :two_pieces_owned => 50, 
+	     :three_pieces_owned => 100, 
+	     :four_pieces_owned => 200
 	   }
 	  }
-
+  }
 		
 end
 
